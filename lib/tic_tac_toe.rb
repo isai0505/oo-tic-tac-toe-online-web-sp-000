21a5b4b58a49d51c3f9c881f293557c70769ff5a
Isai Bismark Cortez <isai0505@gmail.com>
2019-08-24 17:41:23 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -93,9 +93,9 @@ return " #{@board[0]} | #{@board[1]} | #{@board[2]} \n-----------\n
     end
   end
 
-  def play(board)
-    until over?(board) == true
-  turn(board)
+  def play
+    until over? == true
+      turn
   end
 
 
