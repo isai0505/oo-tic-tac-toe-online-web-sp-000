5d62e013ea84857492e592c15f8831cbab467d92
Isai Bismark Cortez <isai0505@gmail.com>
2019-08-24 17:01:36 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -49,7 +49,7 @@ return " #{@board[0]} | #{@board[1]} | #{@board[2]} \n-----------\n
     count
   end
 
-  def current_player
+  def current_player(board)
     turn_count.even? ? "X" : "O"
   end
 
